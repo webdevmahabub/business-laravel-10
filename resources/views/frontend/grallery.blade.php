fdc907330a4cb593bff3e7675baa68ab55f2f12d
webdevmahabub <mahbubbd926@gmail.com>
2023-02-22 08:48:50 +0600

    services

---
+++ resources/views/frontend/grallery.blade.php
@@ -0,0 +1,165 @@
+
+
+<style>
+	
+.container h1 {
+	font-family: 'Raleway';
+	font-weight: 900;
+	text-transform: uppercase;
+	letter-spacing: -1px;
+	font-size: 50px;
+}
+hr {
+	border: 0;
+    height: 3px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+}
+
+.thumbnail {
+	margin-bottom:6px;
+  display:flex;
+  flex-wrap: wrap;
+}
+
+    
+.blur {
+    transform: scale(0.9);
+    opacity: 0.6;
+  	-webkit-filter: blur(2px);
+    -moz-filter: blur(2px);
+    -o-filter: blur(2px);
+    -ms-filter: blur(2px);
+    filter: blur(2px);
+}
+
+.myFooter {
+	display: flex;
+  	flex-flow: row wrap;
+}
+
+footer {
+	background: #000;
+	font-family: 'Raleway', sans-serif;
+	color: rgb(0, 0, 0);
+	max-width: 100%;
+	margin-top: auto;
+}
+
+
+.footer-icons {
+	text-align: center;
+	font-size: 20px;
+	letter-spacing: 5px;
+	width: 100%;
+  	align-items: center;
+  	justify-content: space-around;
+  	position: relative;
+  	margin-top: 15px;
+}
+
+.footer-bottom {
+	text-align: center;
+	color: white;
+	width: 100%;
+  	align-items: center;
+  	justify-content: space-around;
+  	position: relative;
+  	margin-top: 10px;
+  	margin-bottom: 10px;
+}
+a {
+	text-decoration: none;
+}
+a:visited, a:focus{
+	color: white;
+}
+
+footer a:hover {
+	color: rgba(255, 255, 255, 0.6);
+}
+
+</style>
+
+
+<div class="container">
+<h1>Recent Work</h1>
+<hr>
+	<div class="row">
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1494719019271-7bfff81660d2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=74496a215491b1529d6702f146901970&auto=format&fit=crop&w=500&q=60"> </a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523357585206-175e971f2ad9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4d4e050da1c691f01f0643abe9cd5f94&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6 ">
+			<div class="thumbnail">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523359366921-4c14294ff5bd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6010b5b8bc1f6916d652fb48b9e039a6&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523282310917-57c94215f54f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9562049492c4c1c28014be6b4d372fa8&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523279719030-9806e8392152?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83ec2854d04d0f08312dec44bf645e2d&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523264629844-40dd6bf17c2b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c7ff0286ae72f4dfed2bfee5fc8a53fe&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523133892278-d324fa6b326b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1ba6e3178a138e7dcbdd241499378449&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523326350151-473dc3e5215b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aa765415933dbcd55bc27e0726bfd1f5&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="thumbnail img-responsive">
+				<a href="#" title="Image 1"><img src="https://images.unsplash.com/photo-1523086686757-5cda47c78492?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0da31c54dc3e5fd28da519b2cde2c98d&auto=format&fit=crop&w=500&q=60"></a>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="modal" class="modal fade" tabindex="-1" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
+			</div>
+			<div class="modal-body">
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<script>
+	$('.thumbnail').click(function(){
+	$('.modal-body').empty();
+	$($(this).parents('div').html()).appendTo('.modal-body');
+	$('#modal').modal({show:true});
+});
+
+$('#modal').on('show.bs.modal', function () {
+   $('.col-6,.row .thumbnail').addClass('blur');
+})
+
+$('#modal').on('hide.bs.modal', function () {
+   $('.col-6,.row .thumbnail').removeClass('blur');
+})
+</script>
---
+++ resources/views/frontend/services.blade.php
@@ -0,0 +1,99 @@
+
+      <link rel="stylesheet" href="{{ asset('frontend/assets/custome/custom.css') }}">
+            <link rel="stylesheet" href="{{ asset('frontend/assets/custome/fonts.css') }}">
+
+      <link rel="stylesheet" href="{{ asset('frontend/assets/custome/index.css') }}">
+
+      <link rel="stylesheet" href="{{ asset('frontend/assets/custome/fx.css') }}">
+      <link rel="stylesheet" href="{{ asset('frontend/assets/custome/bootstrap.weber.css') }}">
+
+
+<div>       <h2 style="text-align: center"> Our Service</h2>
+
+</div>
+<section id="benefits-3col-12" class="pt-50 pb-md-50 pt-md-0 light spr-edit-el spr-oc-show spr-wout">
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{ asset('frontend/assets/images/weeding.jpg') }}" height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>Wedding Planner & Management</strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{asset('frontend/assets/images/birth.jpg')}}" height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>birthday party Planning </strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{ asset('frontend/assets/images/coprpte.jpg') }}" height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>Corporate Event Management</strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="bg-wrap">
+        <div class="bg"></div>
+    </div>
+<br>  <br><br>
+
+ <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{ asset('frontend/assets/images/weeding.jpg') }}" height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>Wedding Planner & Management</strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{asset('frontend/assets/images/birth.jpg')}}"  height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>birthday party Planning </strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="content-box bg-default shadow mb-50 mt-md--50">
+                    <img class="w-100" src="{{ asset('frontend/assets/images/coprpte.jpg') }}" height="342px;" width="223px;" alt="image">
+                    <div class="padding-x2">
+                        <h4 class="mb-20"><strong>Corporate Event Management</strong></h4>
+                        <p class="mb-30 text-secondary">In our work we try to use only the most modern, convenient and interesting solutions</p>
+                        <a href="#" class="btn btn-secondary">Book Service</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="bg-wrap">
+        <div class="bg"></div>
+    </div>
+
+</section>
+
+
+
